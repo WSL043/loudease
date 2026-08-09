fc0063023cf84206a746f5411d66d43ff169968b
WSL043 <wangsr043@gmail.com>
2026-08-10 03:31:35 +0800

    Align slider E2E with programme target
    Signed-off-by: WSL043 <wangsr043@gmail.com>

--- tools/e2e_slider_persistence.js
+++ tools/e2e_slider_persistence.js
@@ -1,5 +1,6 @@
 process.env.WVB_E2E_CHECK_SLIDER_PERSIST = '1';
-process.env.WVB_E2E_EXPECT = 'reduce';
+process.env.WVB_E2E_EXPECT = 'lift';
+process.env.WVB_E2E_MIN_SIGNAL_TICKS = '40';
 process.env.WVB_E2E_HEADLESS = '1';
 
 require('./e2e_poc_smoke.js');
