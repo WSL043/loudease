892a54f7d510398149f9ab329f8f5ef3983bab4e
WSL043 <wangsr043@gmail.com>
2026-08-12 22:55:32 +0800

    Align slider E2E with stable lift
    Signed-off-by: WSL043 <wangsr043@gmail.com>

--- tools/e2e_slider_persistence.js
+++ tools/e2e_slider_persistence.js
@@ -1,7 +1,9 @@
 process.env.WVB_E2E_CHECK_SLIDER_PERSIST = '1';
 process.env.WVB_E2E_PAGE = 'quiet-dialog.html';
 process.env.WVB_E2E_EXPECT = 'lift';
-process.env.WVB_E2E_MIN_SIGNAL_TICKS = '40';
+// The stable programme policy deliberately needs about four seconds of
+// continuous evidence before applying its full upward correction.
+process.env.WVB_E2E_MIN_SIGNAL_TICKS = '260';
 process.env.WVB_E2E_HEADLESS = '1';
 process.env.WVB_E2E_SILENT_SINK = '1';
 
